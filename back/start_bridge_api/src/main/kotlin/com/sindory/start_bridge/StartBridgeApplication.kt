42e8e38c33e0c44669219df74a8a4fade638ec29
scmoon119 <scmoon119@gmail.com>
2025-08-29 14:09:29 +0900

    first project setting

---
+++ back/start_bridge_api/src/main/kotlin/com/sindory/start_bridge/StartBridgeApplication.kt
@@ -0,0 +1,11 @@
+package com.sindory.start_bridge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StartBridgeApplication
+
+fun main(args: Array<String>) {
+	runApplication<StartBridgeApplication>(*args)
+}
---
+++ back/start_bridge_api/src/test/kotlin/com/sindory/start_bridge/StartBridgeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sindory.start_bridge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartBridgeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
